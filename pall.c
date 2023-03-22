5a9ed068932427fae2313554917e1c9e69cea356
Aynalem A. Nigussie <abebeayni@gmail.com>
2023-03-22 21:46:57 +0300

    push and print values updated

--- pall.c
+++ pall.c
@@ -1,21 +1,42 @@
-#include "monty.h"
-/**
- * f_pall - prints the stack
- * @head: stack head
- * @counter: no used
- * Return: no return
-*/
-void f_pall(stack_t **head, unsigned int counter)
-{
-	stack_t *h;
-	(void)counter;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "./monty.h"
 
-	h = *head;
-	if (h == NULL)
-		return;
-	while (h)
+/**
+ * opcode - function in charge of running builtins
+ * @stack: stack given by main
+ * @str: string to compare
+ * @line_cnt: amount of lines
+ *
+ * Return: nothing
+ */
+void opcode(stack_t **stack, char *str, unsigned int line_cnt)
+{
+	int i = 0;
+
+	instruction_t op[] = INSTRUCTIONS;
+
+	if (!strcmp(str, "stack"))
 	{
-		printf("%d\n", h->n);
-		h = h->next;
+		global.data_struct = 1;
+		return;
 	}
+	if (!strcmp(str, "queue"))
+	{
+		global.data_struct = 0;
+		return;
+	}
+
+	while (op[i].opcode)
+	{
+		if (strcmp(op[i].opcode, str) == 0)
+		{
+			op[i].f(stack, line_cnt);
+			return; /* if we found a match, run the function */
+		}
+		i++;
+	}
+	fprintf(stderr, "L%d: unknown instruction %s\n", line_cnt, str);
+	exit(EXIT_FAILURE);
 }
