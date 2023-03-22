7dc2a11854aa7c7db943fc5bbce5e00032e8f46c
Aynalem A. Nigussie <abebeayni@gmail.com>
2023-03-22 21:41:30 +0300

    main is updated

--- main.c
+++ main.c
@@ -1,45 +1,86 @@
+#define  _POSIX_C_SOURCE 200809L
 #include "monty.h"
-bus_t bus = {NULL, NULL, NULL, 0};
-/**
-* main - monty code interpreter
-* @argc: number of arguments
-* @argv: monty file location
-* Return: 0 on success
-*/
-int main(int argc, char *argv[])
-{
-	char *content;
-	FILE *file;
-	size_t size = 0;
-	ssize_t read_line = 1;
-	stack_t *stack = NULL;
-	unsigned int counter = 0;
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
+void file_error(char *argv);
+void error_usage(void);
+int status = 0;		/* global var declaration */
+
+/**
+ * main - entry point
+ * @argv: list of arguments passed to our program
+ * @argc: amount of args
+ *
+ * Return: nothing
+ */
+int main(int argc, char **argv)
+{
+	FILE *file;
+	size_t buf_len = 0;
+	char *buffer = NULL;
+	char *str = NULL;
+	stack_t *stack = NULL;
+	unsigned int line_cnt = 1;
+
+	global.data_struct = 1;  /* struct defined in monty.h L58*/
 	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
+		error_usage(); /* def in line 82 */
+
 	file = fopen(argv[1], "r");
-	bus.file = file;
+
 	if (!file)
+		file_error(argv[1]);  /* def in line 68 */
+
+	while ((getline(&buffer, &buf_len, file)) != (-1))
 	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	while (read_line > 0)
-	{
-		content = NULL;
-		read_line = getline(&content, &size, file);
-		bus.content = content;
-		counter++;
-		if (read_line > 0)
+		if (status)
+			break;
+		if (*buffer == '\n')
 		{
-			execute(content, &stack, counter, file);
+			line_cnt++;
+			continue;
 		}
-		free(content);
+		str = strtok(buffer, " \t\n");
+		if (!str || *str == '#')
+		{
+			line_cnt++;
+			continue;
+		}
+		global.argument = strtok(NULL, " \t\n");
+		opcode(&stack, str, line_cnt);
+		line_cnt++;
 	}
+	free(buffer);
 	free_stack(stack);
 	fclose(file);
-return (0);
+	exit(EXIT_SUCCESS);
+}
+
+/**
+ * file_error - prints file error message and exits
+ * @argv: argv given by main()
+ *
+ * Desc: print msg if  not possible to open the file
+ * Return: nothing
+ */
+void file_error(char *argv)
+{
+	fprintf(stderr, "Error: Can't open file %s\n", argv);
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * error_usage - prints usage message and exits
+ *
+ * Desc: if user does not give any file or more than
+ * one argument to your program
+ *
+ * Return: nothing
+ */
+void error_usage(void)
+{
+	fprintf(stderr, "USAGE: monty file\n");
+	exit(EXIT_FAILURE);
 }
