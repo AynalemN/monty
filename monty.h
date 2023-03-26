d2fcb74e52e19dccf41bb0e718c072f4696bc0d7
AynalemN <abebeayni@gmail.com>
2023-03-26 04:52:33 -0700

    isascii updated

--- monty.h
+++ monty.h
@@ -25,9 +25,10 @@
 			NULL, NULL      \
 		}                     \
 	}
-extern int isascii();
+extern int isascii(int __c) __THROW;
+
 /**
- * struct stack_s - doubly linked list representation of a stack (or queue)
+ * struct stack_s- doubly linked list representation of a stack (or queue)
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
  * @next: points to the next element of the stack (or queue)
--- pchar.c
+++ pchar.c
@@ -8,9 +8,9 @@
  * pchar - prints the int at the top of the stack as char
  * @stack: stack given by main
  * @line_cnt: amount of lines
- *
  * Return: void
  */
+
 void pchar(stack_t **stack, unsigned int line_cnt)
 {
 	if (!stack || !(*stack))
